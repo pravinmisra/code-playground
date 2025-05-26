450a259177b96c0e71404876e9256231a407509a
Pravin Chandra Misra <praveen.mishra007@gmail.com>
2025-05-26 23:14:13 +0530

    commit_26-05-2025_2

---
+++ multi_dim_array.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 26 22:11:45 2025
+
+@author: Admin
+"""
+
+import numpy as np
+
+arr1 = np.array([12,33,34,24,7])
+
+print(arr1)
+
+print(type(arr1))
+
+arr2 = np.array((1,2,3,4,5))
+
+print(arr2)
+
+print(type(arr2))
+
+arr3 = np.array({1,2,3,4,5,6})
+
+print(arr3)
+
+print(type(arr3))
+
+mdarr1 = np.array([[1,2],[2,3],[3,4]])
+
+print(mdarr1)
+
+print(type(mdarr1))
+
+print(mdarr1.shape)
+
+mdarr2 = np.array([[[1,2,3],[2,3,4]],[[3,4,5],[4,5,6]],[[5,6,7],[6,7,8]]])
+
+print(mdarr2)
+
+print(type(mdarr2))
+
+print(mdarr2.shape)
+                   
+
+
+
+
+
